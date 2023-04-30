f5856ca5c7868231589b25299883975f196bc5db
Sam <sktylr@gmail.com>
2023-04-30 23:21:56 +0100

    added links to socials

--- portfolio/src/components/sidebar/index.js
+++ portfolio/src/components/sidebar/index.js
@@ -3,6 +3,7 @@ import './index.scss'
 import Logo from '../../assets/img/dummy-logo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => {
 	return (
@@ -21,6 +22,18 @@ const Sidebar = () => {
 					<FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
 				</NavLink>
 			</nav>
+			<ul>
+				<li>
+					<a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/sktylr">
+						<FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+					</a>
+				</li>
+				<li>
+					<a target="_blank" rel="noreferrer" href="https://www.github.com/sktylr">
+						<FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+					</a>
+				</li>
+			</ul>
 		</div>
 	)
 }
