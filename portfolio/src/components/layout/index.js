2739431b4789ba4b59084d798b020572f635cab5
Sam <sktylr@gmail.com>
2023-04-30 23:36:54 +0100

    small tag ordering

--- portfolio/src/components/layout/index.js
+++ portfolio/src/components/layout/index.js
@@ -1,11 +1,15 @@
 import './index.scss';
 import Sidebar from '../sidebar/index.js';
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
 	return (
-		<>
+		<div className="App">
 			<Sidebar />
-		</>
+			<div className='page'>
+				<Outlet />
+			</div>
+		</div>
 	)
 }
 
