1a62efb05af9bbc136320e64d06e5b885c35b5e5
Sam <sktylr@gmail.com>
2023-04-30 23:45:22 +0100

    start of home page content

--- portfolio/src/components/home/index.js
+++ portfolio/src/components/home/index.js
@@ -1,7 +1,23 @@
 import './index.scss'
+import Logo from '../../assets/img/dummy-logo.png'
+import { Link } from 'react-router-dom'
 
-const Home = () => (
-	<>Hello</>
-)
+const Home = () => {
+
+	return (
+		<div className="container home-page">
+			<div className="text-zone">
+				<h1> Hi, <br /> I'm
+				<img src={Logo} alt="developer" />
+				am
+				<br />
+				A web developer
+				</h1>
+				<h2> Software Developer with an interest in back-end services</h2>
+				<Link to="/contact" className='flat-button'> CONTACT ME </Link>
+			</div>
+		</div>
+	)
+}
 
 export default Home
