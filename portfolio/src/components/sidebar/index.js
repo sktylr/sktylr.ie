c19c3e2ca845c47ca17deff25ef905eb1ef467ac
Sam <sktylr@gmail.com>
2023-04-30 23:11:17 +0100

    initial links to separate pages

--- portfolio/src/components/sidebar/index.js
+++ portfolio/src/components/sidebar/index.js
@@ -1,6 +1,8 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import Logo from '../../assets/img/dummy-logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => {
 	return (
@@ -8,6 +10,17 @@ const Sidebar = () => {
 			<Link className='logo' to='/'>
 				<img src={Logo} alt="logo" />
 			</Link>
+			<nav>
+				<NavLink exact="true" activeclassname="active" to="/">
+					<FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+				</NavLink>
+				<NavLink exact="true" activeclassname="active" to="/about" className="about-link">
+					<FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+				</NavLink>
+				<NavLink exact="true" activeclassname="active" to="/contact" className="contact-link">
+					<FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+				</NavLink>
+			</nav>
 		</div>
 	)
 }
