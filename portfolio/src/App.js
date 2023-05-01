c299ee6a5e7744dbda60f62e76fc6def3709c3a5
Sam <sktylr@gmail.com>
2023-05-01 20:40:37 +0100

    route to contact page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/layout';
 import Home from './components/home';
 import About from './components/about';
+import Contact from './components/contact';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
       </Route>
     </Routes>
     </>
