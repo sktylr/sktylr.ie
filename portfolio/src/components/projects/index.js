cd2a559621ec70237c151dcf4ebf6ce9c4a279f0
Sam <sktylr@gmail.com>
2023-05-02 00:06:26 +0100

    some setup of the projects page

---
+++ portfolio/src/components/projects/index.js
@@ -0,0 +1,16 @@
+import Loader from 'react-loaders';
+import './index.scss';
+
+const Projects = () => {
+	return (
+		<>
+			<div className="container projects-page">
+				<div className="text-zone">
+				</div>
+			</div>
+			<Loader type="pacman" />
+		</>
+	)
+}
+
+export default Projects
