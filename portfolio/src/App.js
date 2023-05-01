a5b0a31bc87848d3e8334045707d473316c3ec56
Sam <sktylr@gmail.com>
2023-05-01 19:14:09 +0100

    route to about page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/layout';
 import Home from './components/home';
+import About from './components/about';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
       </Route>
     </Routes>
     </>
