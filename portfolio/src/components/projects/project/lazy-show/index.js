73a94a91784647428133354c891a8d0ac9785bc1
Sam <sktylr@gmail.com>
2023-05-03 20:45:36 +0100

    on screen checking

--- portfolio/src/components/projects/project/lazy-show/index.js
+++ portfolio/src/components/projects/project/lazy-show/index.js
@@ -2,6 +2,34 @@ import './index.scss';
 import { motion, useAnimation } from 'framer-motion';
 import { useEffect, useRef, useState } from 'react';
 
+const useOnScreen = (ref, rootMargin = '0px') =>	{
+
+	const [isIntersecting, setIntersecting] = useState(false);
+
+	useEffect(() => {
+		let currentRef = null;
+		const observer = new IntersectionObserver(
+			([entry]) => {
+				setIntersecting(entry.isIntersecting);
+			},
+			{
+				rootMargin
+			}
+		);
+
+		if (ref.current)	{
+			currentRef = ref.current;
+			observer.observe(currentRef);
+		}
+
+		return () => {
+			observer.unobserve(currentRef)
+		}
+	}, [ref, rootMargin]);
+
+	return isIntersecting;
+}
+
 const LazyShow = ({ children }) =>	{
 	const controls = useAnimation();
 	const rootRef = useRef();
