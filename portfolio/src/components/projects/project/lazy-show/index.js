09d44bf3b6fd4edef79b9f84d58ebb0d22d6f98a
Sam <sktylr@gmail.com>
2023-05-03 22:50:39 +0100

    experimenting with transitions

--- portfolio/src/components/projects/project/lazy-show/index.js
+++ portfolio/src/components/projects/project/lazy-show/index.js
@@ -30,29 +30,61 @@ const useOnScreen = (ref, rootMargin = '0px') =>	{
 	return isIntersecting;
 }
 
-const LazyShow = ({ children }) =>	{
+const LazyShow = ({ children, left }) =>	{
+	console.log('LEFT IS ' + left);
 	const controls = useAnimation();
 	const rootRef = useRef();
 	const onScreen = useOnScreen(rootRef);
 
 	useEffect(() => {
 		if (onScreen)	{
-			controls.start({
-				x: 0,
-				opacity: 1,
-				transition:	{
-					duration: 0.8,
-					ease: 'easeOut'
-				}
-			});
+			if (left)	{
+				controls.start({
+					x: 0,
+					opacity: 1,
+					transition:	{
+						duration: 0.8,
+						ease: 'easeIn',
+						delay: 1,
+					}
+				});
+			} else	{
+				console.log('WINDOW WIDTH: ' + window.innerWidth);
+				// controls.start({
+				// 	x: 0,
+				// 	opacity: 1,
+				// 	transition:	{
+				// 		duration: 0.8,
+				// 		ease: 'easeIn',
+				// 		delay: 1,
+				// 		// staggerDirection: -1,
+				// 		from: window.innerWidth / 2,
+				// 		// from: 100
+				// 	}
+				// })
+				controls.start({
+					x: 0,
+					opacity: 1,
+					transition:	{
+						duration: 0.8,
+						ease: 'easeIn',
+						delay: 1,
+						// staggerDirection: -1,
+						// from: window.innerWidth / 2,
+						// from: 100
+					}
+				})
+			}
 		}
-	}, [onScreen, controls]);
+	}, [onScreen, controls, left]);
+
+	const x = left ? -100 : -100; 
 
 	return (
 		<motion.div
 			className='lazy-div'
 			ref={rootRef}
-			initial={{opacity: 0, x: -100}}
+			initial={{opacity: 0, x: x}}
 			animate={controls}
 		>
 			{children}
