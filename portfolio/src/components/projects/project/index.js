487df3d5a2cf4b5f7cf0456284c01f1b5bbc8503
Sam <sktylr@gmail.com>
2024-04-20 22:30:40 +0100

    update image rendering to restrict size

--- portfolio/src/components/projects/project/index.js
+++ portfolio/src/components/projects/project/index.js
@@ -1,57 +1,62 @@
-import './index.scss';
-import LazyShow from './lazy-show';
+import "./index.scss"
+import LazyShow from "./lazy-show"
 
-const locateImage = (path) => (
-	require(`../../../assets/img/${path}`)
-)
+const locateImage = (path) => require(`../../../assets/img/${path}`)
 
-const Text = (text) =>	(
-	<div className='text-side'>
-		{text.map((paragraph, i) => (
-			<p key={i}>
-				{paragraph}
-			</p>
-		))}
-	</div>
+const Text = (text) => (
+    <div className="text-side">
+        {text.map((paragraph, i) => (
+            <p key={i}>{paragraph}</p>
+        ))}
+    </div>
 )
 
 const Image = (image) => (
-	<div className='image-side'>
-		<div>
-			<img src={locateImage(image.source)} alt={image.alt} />
-		</div>
-	</div>
+    <div className="image-side">
+        <div style={{ maxWidth: "200px", maxHeight: "200px" }}>
+            <img
+                src={locateImage(image.source)}
+                alt={image.alt}
+                style={{ maxWidth: "200px", maxHeight: "200px" }}
+            />
+        </div>
+    </div>
 )
 
-
 const Project = ({ projectRaw, index, img }) => {
+    const toRender = []
+    const left = index % 2 === 0
 
-	const toRender = []
-	const left = index % 2 === 0;
+    // optional rendering of a link to a project site (github, client website etc), returns null (renders as blank) if not there
+    const externalLink = () =>
+        projectRaw.link ? (
+            <div className="external-link">
+                <a
+                    href={projectRaw.link.url}
+                    className="flat-button external-link"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    {" "}
+                    {projectRaw.link.alt}{" "}
+                </a>
+            </div>
+        ) : null
 
-	// optional rendering of a link to a project site (github, client website etc), returns null (renders as blank) if not there
-	const externalLink = () => (
-		projectRaw.link ? (
-			<div className='external-link'>
-				<a href={projectRaw.link.url} className='flat-button external-link' target='_blank' rel='noreferrer'> {projectRaw.link.alt} </a>
-			</div>
-		) : null
-	)
+    // choose which side to render the text on
+    if (!left) {
+        toRender.push(Image(img), Text(projectRaw.content))
+    } else toRender.push(Text(projectRaw.content), Image(img))
 
-	// choose which side to render the text on
-	if (!left)	{ 
-		toRender.push(Image(img), Text(projectRaw.content))
-	} else toRender.push(Text(projectRaw.content), Image(img))
-
-	return (
-		<LazyShow left={left}>
-			<h2 className='sub-heading'> {projectRaw.title} </h2>
-			<div className='project-container'>
-				{toRender}
-				{externalLink()}
-			</div>
-		</LazyShow>
-	)
+    return (
+        <LazyShow left={left}>
+            <h2 className="sub-heading"> {projectRaw.title} </h2>
+            <div className="project-container">
+                {toRender}
+                {externalLink()}
+            </div>
+        </LazyShow>
+    )
 }
 
-export default Project;
+export default Project
